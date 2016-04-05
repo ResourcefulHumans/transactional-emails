6dcd68a6412391f321bb8c63dc804c0e340bff66
Markus Tacker <m@cto.hiv>
2016-04-05 14:21:58 +0200

    refactor(package): move email loading logic to index.js

--- build-emails.js
+++ build-emails.js
@@ -3,15 +3,16 @@
 let Promise = require('bluebird')
 Promise.promisifyAll(require('fs'))
 let path = require('path')
-let glob = require('glob')
-let globAsync = Promise.promisify(glob)
 let fs = require('fs')
 let _map = require('lodash/map')
 let _merge = require('lodash/merge')
 let _template = require('lodash/template')
-let _defaults = require('lodash/defaults')
+let _forEach = require('lodash/forEach')
 let juice = require('juice')
 let juiceResourcesAsync = Promise.promisify(juice.juiceResources)
+let showdown = require('showdown')
+let converter = new showdown.Converter()
+let transactionTemplates = require('./')
 
 function run () {
   let templatedata = {
@@ -25,31 +26,7 @@ function run () {
     console.log('  ' + key + ': ' + value)
   })
 
-  return Promise
-    .join(
-      fs.readFileAsync('./emails/templates/layout.html', 'utf8'),
-      fs.readFileAsync('./emails/templates/layout.txt', 'utf8'),
-      globAsync(path.join(__dirname, '/emails/*.json'))
-    )
-    .spread((htmlLayout, textLayout, emails) => {
-      return Promise.map(emails, (email) => {
-        let identifier = path.basename(email, '.json')
-        return Promise.join(
-          fs.readFileAsync('./emails/templates/' + identifier + '.html', 'utf8'),
-          fs.readFileAsync('./emails/templates/' + identifier + '.txt', 'utf8')
-          )
-          .spread((htmlTemplate, textTemplate) => {
-            let e = require(email)
-            return {
-              identifier,
-              subject: e.subject,
-              defaults: _defaults({}, e.defaults),
-              html: htmlLayout.replace('{{content}}', htmlTemplate),
-              text: textLayout.replace('{{content}}', textTemplate)
-            }
-          })
-      })
-    })
+  return transactionTemplates.load()
     .map((email) => {
       return juiceResourcesAsync(
         email.html, {
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+let Promise = require('bluebird')
+Promise.promisifyAll(require('fs'))
+let path = require('path')
+let glob = require('glob')
+let globAsync = Promise.promisify(glob)
+let fs = require('fs')
+let _defaults = require('lodash/defaults')
+
+module.exports = {
+  /**
+   * @returns {Promise}
+   */
+  load: () => {
+    return Promise
+      .join(
+        fs.readFileAsync('./emails/templates/layout.html', 'utf8'),
+        fs.readFileAsync('./emails/templates/layout.txt', 'utf8'),
+        globAsync(path.join(__dirname, '/emails/*.json'))
+      )
+      .spread((htmlLayout, textLayout, emails) => {
+        return Promise.map(emails, (email) => {
+          let identifier = path.basename(email, '.json')
+          return Promise.join(
+            fs.readFileAsync('./emails/templates/' + identifier + '.html', 'utf8'),
+            fs.readFileAsync('./emails/templates/' + identifier + '.txt', 'utf8')
+            )
+            .spread((htmlTemplate, textTemplate) => {
+              let e = require(email)
+              return {
+                identifier,
+                subject: e.subject,
+                defaults: _defaults({}, e.defaults),
+                html: htmlLayout.replace('{{content}}', htmlTemplate),
+                text: textLayout.replace('{{content}}', textTemplate)
+              }
+            })
+        })
+      })
+  }
+}
