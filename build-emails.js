4c7dbca9da23ac256b1c0132dc1225c9d59819fc
Antonio Ballesteros <antonioballesteros@gmail.com>
2021-02-01 16:26:56 +0100

    feat(all): new title2 information

--- build-emails.js
+++ build-emails.js
@@ -12,7 +12,7 @@ let showdown = require('showdown')
 let converter = new showdown.Converter()
 let transactionTemplates = require('./')
 
-function run () {
+function run() {
   let templatedata = {
     webHost: process.env.WEB_HOST || 'http://example.com/'
   }
@@ -20,48 +20,50 @@ function run () {
   console.log()
   console.log('Building emails files …')
   console.log(' data:')
-  _map(templatedata, function (value, key) {
+  _map(templatedata, function(value, key) {
     console.log('  ' + key + ': ' + value)
   })
 
-  return transactionTemplates.load()
-    .map((email) => {
-      // Write to disk for preview
-      let target = path.join(__dirname, '/build/' + email.identifier)
-      console.log('Writing ' + email.identifier, '->', target)
-      let data = {}
-      _merge(data, templatedata)
-      _merge(data, email.defaults)
-      data.subject = _template(email.subject)(data)
-      data.title = _template(email.title)(data)
-      data.type_error = email.type_error
-      let formatContent = (data) => {
-        if (!(typeof data === 'object')) {
-          return data
-        }
-        if (data['@markdown']) {
-          return {
-            '@text': data['@markdown'],
-            '@html': converter.makeHtml(data['@markdown'])
-          }
-        }
-        _forEach(data, (value, key) => {
-          data[key] = formatContent(value)
-        })
+  return transactionTemplates.load().map(email => {
+    // Write to disk for preview
+    let target = path.join(__dirname, '/build/' + email.identifier)
+    console.log('Writing ' + email.identifier, '->', target)
+    let data = {}
+    _merge(data, templatedata)
+    _merge(data, email.defaults)
+    data.subject = _template(email.subject)(data)
+    data.title = _template(email.title)(data)
+    data.title2 = _template(email.title2)(data)
+    data.type_error = email.type_error
+    let formatContent = data => {
+      if (!(typeof data === 'object')) {
         return data
       }
-      formatContent(data)
-      data._ = require('lodash')
-      return Promise.join(
-        fs.writeFileAsync(target + '.html', _template(email.html)(data)),
-        fs.writeFileAsync(target + '.txt', _template(email.text)(data))
-      )
-    })
+      if (data['@markdown']) {
+        return {
+          '@text': data['@markdown'],
+          '@html': converter.makeHtml(data['@markdown'])
+        }
+      }
+      _forEach(data, (value, key) => {
+        data[key] = formatContent(value)
+      })
+      return data
+    }
+    formatContent(data)
+    data._ = require('lodash')
+    return Promise.join(
+      fs.writeFileAsync(target + '.html', _template(email.html)(data)),
+      fs.writeFileAsync(target + '.txt', _template(email.text)(data))
+    )
+  })
 }
 
-run().then(function () {
-  process.exit(0)
-}).catch(function (err) {
-  console.error(err)
-  process.exit(1)
-})
+run()
+  .then(function() {
+    process.exit(0)
+  })
+  .catch(function(err) {
+    console.error(err)
+    process.exit(1)
+  })
--- index.js
+++ index.js
@@ -11,92 +11,108 @@ let juice = require('juice')
 let juiceResourcesAsync = Promise.promisify(juice.juiceResources)
 let handlebars = require('handlebars')
 
-const loadIncludes = () => globAsync(path.join(__dirname, '/emails/includes/*.{txt,html}'))
-  .then((includes) => {
-    const includeData = {
-      txt: [],
-      html: []
+const loadIncludes = () =>
+  globAsync(path.join(__dirname, '/emails/includes/*.{txt,html}')).then(
+    includes => {
+      const includeData = {
+        txt: [],
+        html: []
+      }
+      return Promise.map(includes, include => {
+        return fs.readFileAsync(include, 'utf8').then(data => {
+          const id = include
+            .replace(path.join(__dirname, '/emails/includes/'), '')
+            .replace(/\.[^.]+$/, '')
+          if (/\.txt$/.test(include)) {
+            includeData.txt.push({ id, data })
+          } else {
+            includeData.html.push({ id, data })
+          }
+        })
+      }).then(() => includeData)
     }
-    return Promise
-      .map(includes, (include) => {
-        return fs.readFileAsync(include, 'utf8')
-          .then((data) => {
-            const id = include.replace(path.join(__dirname, '/emails/includes/'), '').replace(/\.[^.]+$/, '')
-            if ((/\.txt$/.test(include))) {
-              includeData.txt.push({id, data})
-            } else {
-              includeData.html.push({id, data})
-            }
-          })
-      })
-      .then(() => includeData)
-  })
+  )
 
 module.exports = {
   /**
    * @returns {Promise}
    */
   load: () => {
-    return Promise
-      .join(
-        fs.readFileAsync(path.join(__dirname, '/emails/templates/layout.html'), 'utf8'),
-        fs.readFileAsync(path.join(__dirname, '/emails/templates/layout.txt'), 'utf8'),
-        globAsync(path.join(__dirname, '/emails/*.json')),
-        loadIncludes()
-      )
-      .spread((htmlLayout, textLayout, emails, includes) => {
-        return Promise.map(emails, (email) => {
-          let identifier = path.basename(email, '.json')
-          let emailConfig = require(email)
-          let template = emailConfig.template ? emailConfig.template : identifier
-          let product = emailConfig.product ? emailConfig.product : 'netwoRHk'
-          return Promise
-            .join(
-              fs.readFileAsync(path.join(__dirname, '/emails/templates/' + template + '.html'), 'utf8')
-                .then((htmlTemplate) => {
-                  return Promise
-                    .map(includes.html, (include) => {
-                      htmlTemplate = htmlTemplate.replace('{{includes.' + include.id + '}}', include.data)
-                    })
-                    .then(() => juiceResourcesAsync(
-                      handlebars.compile(htmlLayout)({
-                        content: htmlTemplate,
-                        product,
-                        area: emailConfig.area,
-                        title: emailConfig.title,
-                        type_error: emailConfig.type_error
-                      }),
-                      {
-                        webResources: {
-                          images: false,
-                          relativeTo: path.join(__dirname, '/emails/')
-                        }
-                      })
-                    )
-                }),
-              fs.readFileAsync(path.join(__dirname, '/emails/templates/' + template + '.txt'), 'utf8')
+    return Promise.join(
+      fs.readFileAsync(
+        path.join(__dirname, '/emails/templates/layout.html'),
+        'utf8'
+      ),
+      fs.readFileAsync(
+        path.join(__dirname, '/emails/templates/layout.txt'),
+        'utf8'
+      ),
+      globAsync(path.join(__dirname, '/emails/*.json')),
+      loadIncludes()
+    ).spread((htmlLayout, textLayout, emails, includes) => {
+      return Promise.map(emails, email => {
+        let identifier = path.basename(email, '.json')
+        let emailConfig = require(email)
+        let template = emailConfig.template ? emailConfig.template : identifier
+        let product = emailConfig.product ? emailConfig.product : 'netwoRHk'
+        return Promise.join(
+          fs
+            .readFileAsync(
+              path.join(__dirname, '/emails/templates/' + template + '.html'),
+              'utf8'
             )
-            .spread((htmlTemplate, textTemplate) => {
-              return Promise
-                .map(includes.txt, (include) => {
-                  textTemplate = textTemplate.replace('{{includes.' + include.id + '}}', include.data)
-                })
-                .then(() => {
-                  return {
-                    identifier,
-                    subject: emailConfig.subject,
-                    defaults: _defaults({}, emailConfig.defaults),
-                    html: htmlTemplate,
-                    text: handlebars.compile(textLayout)({
-                      content: textTemplate,
-                      product,
-                      title: emailConfig.title,
-                      type_error: emailConfig.type_error
-                    })
+            .then(htmlTemplate => {
+              return Promise.map(includes.html, include => {
+                htmlTemplate = htmlTemplate.replace(
+                  '{{includes.' + include.id + '}}',
+                  include.data
+                )
+              }).then(() =>
+                juiceResourcesAsync(
+                  handlebars.compile(htmlLayout)({
+                    content: htmlTemplate,
+                    product,
+                    area: emailConfig.area,
+                    title: emailConfig.title,
+                    title2: emailConfig.title2,
+                    type_error: emailConfig.type_error
+                  }),
+                  {
+                    webResources: {
+                      images: false,
+                      relativeTo: path.join(__dirname, '/emails/')
+                    }
                   }
-                })
-            })
+                )
+              )
+            }),
+          fs.readFileAsync(
+            path.join(__dirname, '/emails/templates/' + template + '.txt'),
+            'utf8'
+          )
+        ).spread((htmlTemplate, textTemplate) => {
+          return Promise.map(includes.txt, include => {
+            textTemplate = textTemplate.replace(
+              '{{includes.' + include.id + '}}',
+              include.data
+            )
+          }).then(() => {
+            return {
+              identifier,
+              subject: emailConfig.subject,
+              defaults: _defaults({}, emailConfig.defaults),
+              html: htmlTemplate,
+              text: handlebars.compile(textLayout)({
+                content: textTemplate,
+                product,
+                title: emailConfig.title,
+                title2: emailConfig.title2,
+                type_error: emailConfig.type_error
+              })
+            }
+          })
         })
       })
+    })
   }
 }
